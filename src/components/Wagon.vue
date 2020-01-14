962a553942430d656428bc3dba1e845db8004002
Kumsal Obuz <info@viroidgames.com>
2020-01-14 16:56:22 -0500

    fixes more layout concerns

--- src/components/Wagon.vue
+++ src/components/Wagon.vue
@@ -1,68 +1,82 @@
 <template>
-  <v-container class="node">
-    <v-col>
-      <fieldset class="controlsFieldset">
-        <legend>Controls</legend>
-        <div class="controls">
-          <button color="green" onClick="createNode">
-            <v-icon>fas fa-plus</v-icon>
-          </button>
-          <button color="red" onClick="deleteNode" :disabled="first">
-            <v-icon>fas fa-minus</v-icon>
-          </button>
-          <button onClick="moveNode" :disabled="first">
-            <v-icon>fas fa-arrow-up</v-icon>
-          </button>
-          <button onClick="moveNode" :disabled="last">
-            <v-icon>fas fa-arrow-down</v-icon>
-          </button>
-          <button onClick="toggleNode">
-            <v-icon v-if="regexNode.active">fas fa-check-square</v-icon>
-            <v-icon v-else>fas fa-square</v-icon>
-          </button>
-        </div>
-      </fieldset>
-      <div class="flags">
-        <label v-for="flag in ['g', 'i', 'm']" :key="flag">
-          <v-checkbox
-            name="flag"
-            :disabled="!regexNode.active"
-            :checked="regexNode.flags[flag]"
-            onChange="toggleFlag"
-          />
-          ({{ flag }})
-        </label>
-      </div>
-    </v-col>
-    <v-col>
-      <v-row>
-        <fieldset class="patternFieldset">
-          <legend>Pattern</legend>
-          <div class="pattern">
-            /
+  <v-container class="node my-2">
+    <v-row>
+      <v-col lg="2">
+        <h3>Controls</h3>
+        <v-divider />
+        <v-container>
+          <v-row>
+            <v-col class="d-flex align-center justify-space-between">
+              <v-btn x-small fab color="green" onClick="createNode">
+                <v-icon>fas fa-plus</v-icon>
+              </v-btn>
+              <v-btn x-small fab color="red" onClick="deleteNode" :disabled="first">
+                <v-icon>fas fa-minus</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn x-small fab color="primary--text" onClick="moveNode" :disabled="first">
+                <v-icon>fas fa-chevron-up</v-icon>
+              </v-btn>
+              <v-btn x-small fab color="primary--text" onClick="moveNode" :disabled="last">
+                <v-icon>fas fa-chevron-down</v-icon>
+              </v-btn>
+              <v-btn icon onClick="toggleNode">
+                <v-icon v-if="regexNode.active">fas fa-check-square</v-icon>
+                <v-icon v-else>fas fa-square</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="flags d-flex align-center justify-space-between"> </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+      <v-col>
+        <h3>Pattern</h3>
+        <v-divider />
+        <v-container>
+          <v-row class="align-start">
+            <span class="headline">/</span>
             <v-text-field
-              name="pattern"
+              dense
+              full-width
+              clearable
+              hide-details
               placeholder="put your pattern here: \d"
               :disabled="!regexNode.active"
               :value="regexNode.pattern !== null && patternStringify(decodeURI(regexNode.pattern))"
-              onChange="updatePattern"
             />
-            /
-          </div>
-        </fieldset>
-      </v-row>
-      <v-row>
-        <fieldset class="replaceFieldset">
-          <legend>Replace</legend>
-          <v-text-field
-            class="replace"
-            :disabled="!regexNode.active"
-            :value="regexNode.replace"
-            onChange="updateReplace"
-          />
-        </fieldset>
-      </v-row>
-    </v-col>
+            <span class="headline">/</span>
+            <label v-for="flag in ['g', 'i', 'm']" :key="flag">
+              <v-checkbox
+                dense
+                hide-details
+                class="mx-1 my-0"
+                :disabled="!regexNode.active"
+                :checked="regexNode.flags[flag]"
+                :label="`(${flag})`"
+              />
+            </label>
+          </v-row>
+        </v-container>
+        <h3>Replace</h3>
+        <v-divider />
+        <v-container>
+          <v-row>
+            <v-text-field
+              dense
+              full-width
+              clearable
+              hide-details
+              placeholder="place the text you want to use as replacement: $1, some other text or leave empty"
+              :disabled="!regexNode.active"
+              :value="regexNode.replace"
+            />
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -76,7 +90,9 @@ export default {
     deleteNode() {},
     createNode() {},
     toggleNode() {},
-    toggleFlag() {},
+    toggleFlag() {
+      console.log("sikerler");
+    },
     updatePattern() {},
     updateReplace() {}
   }
@@ -86,45 +102,29 @@ export default {
 <style>
 .node {
   border-radius: 3px;
-  background-color: #666;
-  /* border: 1px solid rgba(173, 65, 61, .4); */
-  text-align: center;
-  padding: 8px;
-  display: grid;
+  border: 1px solid #e6e6e6;
+  /* display: grid;
   grid-gap: 0.5%;
-  grid-template-columns: 8% 66% 25%;
+  position: relative; */
+}
+/* .buttons {
+  align-content: center;
+  justify-content: space-between;
+} */
+/* 
+.controls {
+  border-radius: 3px;
+  border-width: 1px solid #d0d0d0;
+  background: #e2e4e6;
   position: relative;
-  margin: 10px 0;
-  z-index: 0;
-  width: 98%;
-}
-legend {
-  font-size: 1.4em;
-  border-radius: 3px;
-  background: #e2e4e6;
-  padding: 2px 6px;
-  text-align: left;
-}
-fieldset {
-  padding: 1% 2%;
-  border-color: #d0d0d0;
-  border-style: solid;
-  border-radius: 3px;
-  border-width: 1px;
-  background: #e2e4e6;
 }
 
-.controlsFieldset {
-  /* grid-area: 2 / 1 / 2 / 3; */
-  /*   */
-}
-label {
-  margin: 0 5px;
-  font-size: 1.4em;
+.patternFieldset {
+  width: inherit;
+  outline: 1px solid red;
 }
 
 .pattern {
-  font-size: 3.6em;
   display: grid;
   grid-template-columns: 2% 92% 2%;
   grid-gap: 2%;
@@ -136,12 +136,11 @@ label {
   color: #444444;
   border-radius: 3px;
   padding: 4px;
-  font-size: 2rem;
   border: 0;
   outline: 0;
   height: 32px;
 }
-.replace {
+.replace1 {
   width: 96%;
 }
 
@@ -152,36 +151,8 @@ label {
   justify-content: center;
 }
 
-button {
-  padding: 0;
-  border: 0;
-  margin: 2px 3px;
-  background: transparent;
-  color: #007fff;
-  outline: 0;
-}
-button svg {
-  width: 24px;
-  height: 24px;
-  border-radius: 50%;
-}
-.move-up,
-.move-down {
-  background: white;
-}
-.clone {
-  background: #59ac44;
-  color: white;
-}
-.disable {
-  color: #444444;
-}
-.delete {
-  background: rgba(173, 65, 61);
-  color: white;
-}
 
 :disabled {
   opacity: 0.3;
-}
+} */
 </style>
